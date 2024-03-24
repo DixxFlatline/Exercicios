829f338e5c98b5c2dde35248cd218ff61eb82451
Rafael <99206986+DixxFlatline@users.noreply.github.com>
2024-03-24 13:28:32 -0300

    Create bee1003.java

---
+++ Beecrowd/Java/bee1003.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int SOMA = A + B;
+		
+		System.out.printf("SOMA = %d%n", SOMA);
+		sc.close();
+
+	}
+
+}
