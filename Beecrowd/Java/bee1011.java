403038082a29e79f1432ef1133fbab51413df5f4
Rafael <99206986+DixxFlatline@users.noreply.github.com>
2024-03-24 21:25:11 -0300

    Create bee1011.java

---
+++ Beecrowd/Java/bee1011.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double raio = sc.nextDouble();
+		double pi = 3.14159;
+		double volume = (4/3.0) * pi* Math.pow(raio, 3);
+				
+		System.out.printf("VOLUME = %.3f%n", volume);
+		sc.close();
+
+	}
+}
