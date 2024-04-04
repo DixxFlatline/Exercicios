994d901918f9f095acbc15ea50d7383251324e91
Rafael <99206986+DixxFlatline@users.noreply.github.com>
2024-04-03 21:32:45 -0300

    Create bee1016.java

---
+++ Beecrowd/Java/bee1016.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int distancia = sc.nextInt();
+		
+		System.out.printf("%d minutos%n", distancia * 2);
+		
+		sc.close();
+	}
+}
